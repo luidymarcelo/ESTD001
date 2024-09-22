41092259b929b102099db469ad77e6614fa01328
Luidy <luidy.oliveira@itacorda.com.br>
2024-09-22 09:00:49 -0300

    Commit Exercício Cap.04

---
+++ Lógica de Programação e Algoritmos com JavaScript - Edécio Lepsen/Cap.04/Ex1.js
@@ -0,0 +1,25 @@
+// CRIA REFERÊNCIA AOS ELEMENTOS DE RESPOSTA DO PROGRAMA
+const frm   = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+// CRIA O "OUVINTE", ACIONADO QUANDO O BOTÃO SUBMIT FOR CLICADO
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()                               // EVITA O ENVIO DO FORMULÁRIO
+    const nome       = frm.inNome.value              // OBTÉM VALORES DO FORM
+    const nota1      = Number(frm.inNota1.value)
+    const nota2      = Number(frm.inNota2.value)
+    const media      = (nota1 + nota2) / 2
+
+    resp1.innerText  = `Média no aluno ${nome} é ${media.toFixed(2)}`
+
+    if (media >= 7){
+    resp2.innerText   = `Parabéns! Você foi aprovado`
+    resp2.style.color = "Green"
+    }
+
+    if (media < 7){
+    resp2.innerText   = `Parabéns seu burro, você foi reprovado`
+    resp2.style.color = "Red"
+    }
+})
